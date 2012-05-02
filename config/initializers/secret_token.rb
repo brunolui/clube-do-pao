990823786385e3e5fb01b5b902298586f32ca38e
Bruno Lui <bruno.lui@bluesoft.com.br>
2012-05-02 19:33:58 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClubeDoPao::Application.config.secret_token = '4a1176cf283a191898469cb0687b9b8ad0c0f119006e213a9f54b106bf8a7b8ce524b03d97abcf748cdeae6d092a6c92f15384e93e4199c5c60003c03bd7348e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClubeDoPao::Application.config.session_store :cookie_store, key: '_clube-do-pao_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClubeDoPao::Application.config.session_store :active_record_store
