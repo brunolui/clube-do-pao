b9efbc38f3a5437380dbfd9b8d9267caac4a9eee
Bruno Lui <bruno.lui@bluesoft.com.br>
2012-05-04 18:45:37 -0300

    Sending welcome email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,11 @@
+# encoding: utf-8
 class Notifier < ActionMailer::Base
   default :from => "notifications@example.com"
 
   def welcome_email(member)
     @member = member
     @url  = "http://example.com/login"
-    mail(:to => member.email, :subject => "Welcome to Clube do pão")
+    mail(:to => member.email, :subject => "Clube do pão")
   end
   
 end
