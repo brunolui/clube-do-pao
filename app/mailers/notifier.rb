4e3f76cbf9bb9e4c0aadad6bc9e0a6b438c1961e
Bruno Lui <bruno.lui@bluesoft.com.br>
2012-05-28 09:28:54 -0300

    solving deploy problems

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,15 +1,15 @@
-# encoding: utf-8
+
 class Notifier < ActionMailer::Base
   default :from => "notifications@example.com"
 
   def welcome_email(member)
     @member = member
     @url  = "http://example.com/login"
-    mail(:to => member.email, :subject => "Clube do pão")
+    mail(:to => member.email, :subject => "Clube do pao")
   end
   
   def event_email(event)
     @event = event
-    mail(:to => event.member.email, :subject => "Seu dia do Clube do pão")
+    mail(:to => event.member.email, :subject => "Seu dia do Clube do pao")
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,16 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
 ClubeDoPao::Application.configure do
 
-config.action_mailer.delivery_method = :sendmail
 # Defaults to:
 # config.action_mailer.sendmail_settings = {
 #   :location => '/usr/sbin/sendmail',
 #   :arguments => '-i -t'
 # }
+config.action_mailer.delivery_method = :sendmail
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
    :address => "smtp.gmail.com",
@@ -19,9 +21,6 @@ config.action_mailer.smtp_settings = {
    :password => "bluesoft8437",
    :enable_starttls_auto => true
 }
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ClubeDoPao::Application.initialize!
